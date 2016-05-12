8af0400aca007d0dac8e8e800d874210febf9440
Luis Conceicao <lconceicao@ubiwhere.com>
2016-05-12 12:44:54 +0100

    Added header messages to main script

--- int-sdk/run-tests.sh
+++ int-sdk/run-tests.sh
@@ -3,14 +3,15 @@
 set -xe
 
 #cleanup
-#./cleanup.sh
-./scripts/clean-catalogue-server.sh 127.0.0.1 4012
+scripts/clean-catalogue-server.sh 127.0.0.1 4012
 
 # Prepare working environment
 mkdir -p workspaces projects packages
 
 
+
 # son-emu
+scripts/print-header.sh "Testing: Workspace + Project + Dep-Project, push to SON-EMU"
 scripts/1_create_son_workspace.sh workspaces/ws1
 scripts/2_standalone-project-y1.sh workspaces/ws1 projects/prj1 resources/project-Y1-emu.zip
 scripts/3_dependent-project-y1.sh workspaces/ws1 projects/prj2 resources/project-Y1-emu.zip
@@ -20,6 +21,7 @@ scripts/clean-catalogue-server.sh 127.0.0.1 4012
 
 
 # son-sp
+scripts/print-header.sh "Testing: Workspace + Project + Dep-Project, push to SON-SP"
 scripts/1_create_son_workspace.sh workspaces/ws2
 scripts/2_standalone-project-y1.sh workspaces/ws2 projects/prj3 resources/project-Y1-sp.zip
 scripts/3_dependent-project-y1.sh workspaces/ws2 projects/prj4 resources/project-Y1-sp.zip
