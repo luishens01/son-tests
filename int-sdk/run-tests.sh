47783957079997fb1807c48f278b9077e66b61bf
Luis Conceicao <lconceicao@ubiwhere.com>
2016-05-19 11:12:53 +0100

    Added son-push to the scripts. The first set of projects (prj1 and prj2) will be pushed to emu dummy gatekeeper, which will be running in a docker container. The container for SP gatekeepr is not yet created, so the push is configured to a fake url: 'sp.gatekeeper.com:8000' which will result in an push failure for now.

--- int-sdk/run-tests.sh
+++ int-sdk/run-tests.sh
@@ -14,8 +14,8 @@ mkdir -p workspaces projects packages
 # son-emu
 scripts/print-header.sh "Testing: Workspace + Project + Dep-Project, push to SON-EMU"
 scripts/1_create_son_workspace.sh workspaces/ws1
-scripts/2_standalone-project-y1.sh workspaces/ws1 projects/prj1 resources/project-Y1-emu.zip
-scripts/3_dependent-project-y1.sh workspaces/ws1 projects/prj2 resources/project-Y1-emu.zip
+scripts/2_standalone-project-y1.sh workspaces/ws1 projects/prj1 http://127.0.0.1:8001 resources/project-Y1-emu.zip
+scripts/3_dependent-project-y1.sh workspaces/ws1 projects/prj2 http://127.0.0.1:8001 resources/project-Y1-emu.zip
 
 # clean catalogues
 scripts/clean-catalogue-server.sh 127.0.0.1 4012
@@ -24,7 +24,7 @@ scripts/clean-catalogue-server.sh 127.0.0.1 4012
 # son-sp
 scripts/print-header.sh "Testing: Workspace + Project + Dep-Project, push to SON-SP"
 scripts/1_create_son_workspace.sh workspaces/ws2
-scripts/2_standalone-project-y1.sh workspaces/ws2 projects/prj3 resources/project-Y1-sp.zip
-scripts/3_dependent-project-y1.sh workspaces/ws2 projects/prj4 resources/project-Y1-sp.zip
+scripts/2_standalone-project-y1.sh workspaces/ws2 projects/prj3 http://sp.gatekeeper.com:8000 resources/project-Y1-sp.zip
+scripts/3_dependent-project-y1.sh workspaces/ws2 projects/prj4 http://sp.gatekeeper.com:8000 resources/project-Y1-sp.zip
 
 
