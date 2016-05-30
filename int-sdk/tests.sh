73147fab6eaf9c091a698a5261eb31fca1930011
Luis Conceicao <lconceicao@ubiwhere.com>
2016-05-30 14:54:30 +0100

    Cleanup mininet interfaces before running son-emu

--- int-sdk/tests.sh
+++ int-sdk/tests.sh
@@ -11,21 +11,23 @@ cd ..
 docker ps -a
 
 
+# run son-emu in a docker container in the background, expose fake GK and management API
+sudo int-sdk/scripts/rm_container.sh son-emu-int-test
+sudo docker run -d -i --name 'son-emu-int-test' --net='host' --pid='host' --privileged='true' \
+    -v '/var/run/docker.sock:/var/run/docker.sock' \
+    -p 5000:5000 \
+    -p 4242:4242 \
+    registry.sonata-nfv.eu:5000/son-emu
+
+sudo docker exec son-emu-int-test mn --clean
+sudo docker exec son-emu-int-test python src/emuvim/examples/sonata_y1_demo_topology_1.py
+
+
 # run son-cli in a docker container
 sudo int-sdk/scripts/rm_container.sh son-cli-int-test
 sudo docker run -d -i --name 'son-cli-int-test' --net='host' --pid='host' --privileged='true' \
     -v '/var/run/docker.sock:/var/run/docker.sock' \
     registry.sonata-nfv.eu:5000/son-cli
-
-# run son-emu in a docker container in the background, expose fake GK and management API
-sudo int-sdk/scripts/rm_container.sh son-emu-int-test
-sudo docker run -d -i --name 'son-emu-int-test' --net='host' --pid='host' --privileged='true' \
-    -v '/var/run/docker.sock:/var/run/docker.sock' \
-    -p 5050:5000 \
-    -p 4242:4242 \
-    registry.sonata-nfv.eu:5000/son-emu 'python src/emuvim/examples/sonata_y1_demo_topology_1.py'
-
-
 
 
 # prepare son-cli-int-test container for tests
