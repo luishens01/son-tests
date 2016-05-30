c537a3be5fffc37099c095e6fe71dd11060be01b
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-05-30 08:40:44 +0200

    Changing scp by docker cp

--- int-bss-gkeeper/deployment.sh
+++ int-bss-gkeeper/deployment.sh
@@ -16,7 +16,6 @@ sleep 5
 
 # -- BSS
 if ! [[ "$(docker inspect -f {{.State.Running}} son-bss 2> /dev/null)" == "" ]]; then docker rm -fv son-bss ; fi
-docker run -d --name son-bss -p 25001:1337 -p 25002:1338 -v "$(pwd)/reports:/usr/local/yeoman/SonataBSS/E2E_tests/reports" -it registry.sonata-nfv.eu:5000/son-yo-gen-bss
-docker exec -t son-bss sudo chown yeoman:yeoman /usr/local/yeoman/SonataBSS/E2E_tests/reports
+docker run -d --name son-bss -p 25001:1337 -p 25002:1338 -it registry.sonata-nfv.eu:5000/son-yo-gen-bss
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
--- int-bss-gkeeper/tests.sh
+++ int-bss-gkeeper/tests.sh
@@ -4,13 +4,11 @@ docker exec -t -d son-bss grunt serve:integration_tests --suite=intBSS_GK
 
 # -- get the remote reports
 x=0
-scp -rp jenkins@sp.int2.sonata-nfv.eu:/var/lib/jenkins/jobs/int-bss-gkeeper/workspace/int-bss-gkeeper/reports/htmlReport.html htmlReport.html
+docker cp son-bss:/usr/local/yeoman/SonataBSS/E2E_tests/reports .
 while [ "$x" -lt 100 -a ! -e $(pwd)/reports/htmlReport.html ]; do
-        x=$((x+1))
-		scp -rp jenkins@sp.int2.sonata-nfv.eu:/var/lib/jenkins/jobs/int-bss-gkeeper/workspace/int-bss-gkeeper/reports/htmlReport.html htmlReport.html
+        x=$((x+1))		
         sleep 1
+		docker cp son-bss:/usr/local/yeoman/SonataBSS/E2E_tests/reports .
 done
-scp -rp jenkins@sp.int2.sonata-nfv.eu:/var/lib/jenkins/jobs/int-bss-gkeeper/workspace/int-bss-gkeeper/reports reports
-
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
