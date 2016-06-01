3066ee7051b9954cd5514820a6b4a555218b82e8
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-06-01 12:46:39 +0200

    Changing environment parameters

--- int-bss-gkeeper/deployment.sh
+++ int-bss-gkeeper/deployment.sh
@@ -12,24 +12,24 @@ docker pull registry.sonata-nfv.eu:5000/son-catalogue-repos
 #Databases:
 #Postgres
 docker run -d -p 5432:5432 --name son-postgres -e POSTGRES_DB=gatekeeper -e POSTGRES_USER=sonatatest -e POSTGRES_PASSWORD=sonata ntboes/postgres-uuid
-while ! nc -z sp.int3.sonata-nfv.eu 5432; do
+while ! nc -z sp.int2.sonata-nfv.eu 5432; do
   sleep 1 && echo -n .; # waiting for postgres
 done;
 #Mongo
 docker run -d -p 27017:27017 --name son-mongo mongo
-while ! nc -z sp.int3.sonata-nfv.eu 27017; do
+while ! nc -z sp.int2.sonata-nfv.eu 27017; do
   sleep 1 && echo -n .; # waiting for mongo
 done;
 
 #Gatekeeper
 echo gtksrv
 echo populate database
-docker run -i -e DATABASE_HOST=sp.int3.sonata-nfv.eu -e RACK_ENV=integration -e DATABASE_HOST=sp.int3.sonata-nfv.eu -e DATABASE_PORT=5432 -e POSTGRES_PASSWORD=sonata -e POSTGRES_USER=sonatatest --rm=true registry.sonata-nfv.eu:5000/son-gtksrv bundle exec rake db:migrate
+docker run -i -e DATABASE_HOST=sp.int2.sonata-nfv.eu -e RACK_ENV=integration -e DATABASE_HOST=sp.int2.sonata-nfv.eu -e DATABASE_PORT=5432 -e POSTGRES_PASSWORD=sonata -e POSTGRES_USER=sonatatest --rm=true registry.sonata-nfv.eu:5000/son-gtksrv bundle exec rake db:migrate
 echo 
-docker run --name son-gtksrv -d -p 5300:5300 --add-host sp.int.sonata-nfv.eu:10.31.11.33 --add-host jenkins.sonata-nfv.eu:10.31.11.33 --link son-broker --link son-postgres -e RACK_ENV=integration -e DATABASE_HOST=sp.int3.sonata-nfv.eu -e DATABASE_PORT=5432 -e POSTGRES_PASSWORD=sonata -e POSTGRES_USER=sonatatest -e MQSERVER=amqp://guest:guest@10.31.11.33:5672 -e RACK_ENV=integration registry.sonata-nfv.eu:5000/son-gtksrv
+docker run --name son-gtksrv -d -p 5300:5300 --add-host sp.int.sonata-nfv.eu:10.31.11.33 --add-host jenkins.sonata-nfv.eu:10.31.11.33 --link son-broker --link son-postgres -e RACK_ENV=integration -e DATABASE_HOST=sp.int2.sonata-nfv.eu -e DATABASE_PORT=5432 -e POSTGRES_PASSWORD=sonata -e POSTGRES_USER=sonatatest -e MQSERVER=amqp://guest:guest@10.31.11.33:5672 -e RACK_ENV=integration registry.sonata-nfv.eu:5000/son-gtksrv
 echo gtkapi
 docker pull registry.sonata-nfv.eu:5000/son-gtkapi
-docker run --name son-gtkapi -d -p 32001:5000 --add-host sp.int.sonata-nfv.eu:10.31.11.33 --link son-gtkpkg --link son-gtksrv -e RACK_ENV=integration -e PACKAGE_MANAGEMENT_URL=http://sp.int3.sonata-nfv.eu:5100 -e SERVICE_MANAGEMENT_URL=http://sp.int3.sonata-nfv.eu:5300 registry.sonata-nfv.eu:5000/son-gtkapi 
+docker run --name son-gtkapi -d -p 32001:5000 --add-host sp.int.sonata-nfv.eu:10.31.11.33 --link son-gtkpkg --link son-gtksrv -e RACK_ENV=integration -e PACKAGE_MANAGEMENT_URL=http://sp.int2.sonata-nfv.eu:5100 -e SERVICE_MANAGEMENT_URL=http://sp.int2.sonata-nfv.eu:5300 registry.sonata-nfv.eu:5000/son-gtkapi 
 
 #Catalogues
 docker run --name son-catalogue-repos -d -p 4002:4011 --add-host mongo:10.31.11.33 registry.sonata-nfv.eu:5000/son-catalogue-repos
