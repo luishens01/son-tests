90712182fb249ed3d60cc3b8113d04cd29619ba6
shuaibsiddiqui <shuaib.siddiqui@i2cat.net>
2016-06-02 13:08:39 +0200

    int-slm-infrabstract update

--- int-slm-infrabstract/deployment.sh
+++ int-slm-infrabstract/deployment.sh
@@ -14,9 +14,5 @@ sleep 10
 int-slm-infrabstract/scripts/msg2infrabstract.sh
 sleep 180
 
-# -- BSS
-#if ! [[ "$(docker inspect -f {{.State.Running}} son-bss 2> /dev/null)" == "" ]]; then docker rm -fv son-bss ; fi
-#docker run -d --name son-bss -p 25001:1337 -p 25002:1338 -it registry.sonata-nfv.eu:5000/son-yo-gen-bss
-
 export DOCKER_HOST="unix:///var/run/docker.sock"
 
--- int-slm-infrabstract/tests.sh
+++ int-slm-infrabstract/tests.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 #WORK IN PROGRESS
-export DOCKER_HOST="tcp://sp.int2.sonata-nfv.eu:2375"
-docker exec -t -d son-bss grunt serve:integration_tests --suite=intBSS_GK
-
-# -- get the remote reports
-x=0
-docker cp son-bss:/usr/local/yeoman/SonataBSS/E2E_tests/reports .
-while [ "$x" -lt 100 -a ! -e $(pwd)/reports/htmlReport.html ]; do
-        x=$((x+1))
-        sleep 1
-                docker cp son-bss:/usr/local/yeoman/SonataBSS/E2E_tests/reports .
-done
-
-export DOCKER_HOST="unix:///var/run/docker.sock"
 
