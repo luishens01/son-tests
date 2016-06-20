62e4efc9ea251db25fe549f8a5c2574fbe5107cd
Luis Conceicao <lconceicao@ubiwhere.com>
2016-06-20 16:52:41 +0100

    Removed unnecessary scripts

--- int-sdk/test-push-emu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# execute tests
-#printheader "EXECUTE SON-CLI INTEGRATION TESTS & PUSH TO EMU-GK"
-#docker exec son-cli-int-test /bin/bash -c 'cd /tests; ./run-test-push-emu.sh'
--- int-sdk/test-push-sp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# execute tests
-printheader "EXECUTE SON-CLI INTEGRATION TESTS & PUSH TO REAL SP-GATEKEEPER"
-docker exec son-cli-int-test /bin/bash -c 'cd /tests; ./run-test-push-sp.sh'
