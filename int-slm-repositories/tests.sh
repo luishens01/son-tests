0e284c06915e68c5ee27b0717a9eca1dff9dbc91
Daniel <dang03@hotmail.com>
2016-07-07 14:13:54 +0100

    Update tests.sh

--- int-slm-repositories/tests.sh
+++ int-slm-repositories/tests.sh
@@ -1,1 +1,11 @@
 #WORK IN PROGRESS
+#!/bin/bash
+# Contact Repositories-DB
+status_code=$(curl -s -o /dev/null -w "%{http_code}" http://sp.int3.sonata-nfv.eu:27017/)
+
+if [[ $status_code != 20* ]] ;
+  then
+    echo "Error: Response error $status_code"
+    exit -1
+fi
+echo "Success: Repositories-DB found"
