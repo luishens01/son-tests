e1ab7fd8d41fe1dc24b2351e42200ebb55eb6c90
Daniel <dang03@hotmail.com>
2016-07-08 13:47:42 +0100

    Update postVNFR2NFRepo.sh

--- int-slm-repositories/scripts/postVNFR2NFRepo.sh
+++ int-slm-repositories/scripts/postVNFR2NFRepo.sh
@@ -3,7 +3,7 @@
 printf "\n\n======== POST VNFR to VNF repository ========\n\n\n"
 # -- insert VNFR
 # curl -H "Content-Type: application/json" -X POST -d @int-slm-repositories/resources/vnfr.json http://api.int.sonata-nfv.eu:4002/records/vnfr/vnf-instances
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X POST -F @int-slm-repositories/resources/vnfr.json http://api.int.sonata-nfv.eu:4002/records/vnfr/vnf-instances)
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X POST -d @int-slm-repositories/resources/vnfr.json http://api.int.sonata-nfv.eu:4002/records/vnfr/vnf-instances)
 echo $resp
 
 code=$(echo "$resp" | tail -n1)
