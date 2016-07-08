da99af56d15e5cce5624319b6a064f0891f880a2
Daniel <dang03@hotmail.com>
2016-07-08 13:43:11 +0100

    Update deployment.sh

--- int-slm-repositories/deployment.sh
+++ int-slm-repositories/deployment.sh
@@ -15,7 +15,48 @@ while ! nc -z sp.int3.sonata-nfv.eu 27017; do
   sleep 1 && echo -n .; # waiting for mongo
 done;
 
-# -- SLM
+# MySQL (Monitoring)
+# Clean database
+# Removing
+docker rm -fv son-monitor-mysql
+sleep 5
+# Starting
+docker run -d -p 3306:3306 --name son-monitor-mysql registry.sonata-nfv.eu:5000/son-monitor-mysql
+while ! nc -z sp.int3.sonata-nfv.eu 27017; do
+  sleep 1 && echo -n .; # waiting for mysql
+done;
+
+# Check if RabbitMQ is running
+if ! nc -z sp.int3.sonata-nfv.eu 5672; then
+  echo "Initializing RabbitMQ docker"
+  docker run -d -p 5672:5672 --name son-broker rabbitmq:3
+  while ! nc -z sp.int3.sonata-nfv.eu 5672; do
+    sleep 1 && echo -n .; # waiting for rabbitmq
+  done;
+fi
+
+# Check if NSR/VNFR repositories are running
+if ! nc -z sp.int3.sonata-nfv.eu 4002; then
+  echo "Initializing Repositories"
+  docker run -d -p 4002:4002  --name son-catalogue-repos registry.sonata-nfv.eu:5000/son-catalogue-repos
+  while ! nc -z sp.int3.sonata-nfv.eu 4002; do
+    sleep 1 && echo -n .; # waiting for repositories
+  done;
+fi
+
+# Check if SLM is running
+is_running=`docker inspect -f {{.State.Running}} servicelifecyclemanagement`;
+if [ "$is_running" = "false" ]; then
+  echo "Initializing SLM"
+  docker run -d --name servicelifecyclemanagement registry.sonata-nfv.eu:5000/servicelifecyclemanagement
+  while [ "$is_running" = "false" ]; do
+    sleep 1 && echo -n .; # waiting for SLM
+    is_running=`docker inspect -f {{.State.Running}} servicelifecyclemanagement`;
+  done
+fi
+
+## TODO Add Monitoring dockers
+
 export DOCKER_HOST="unix:///var/run/docker.sock"
 
 #DEPLOYMENT
