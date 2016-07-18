87662035ad5918d4bd65436ff421734b5c649270
felipevicens <felipe.vicens@atos.net>
2016-07-18 14:36:35 +0200

    Adding error control

--- int-bss-gkeeper/tests.sh
+++ int-bss-gkeeper/tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -x
-set -e
 export DOCKER_HOST="tcp://sp.int3.sonata-nfv.eu:2375"
 docker rm -fv son-bss
+set -x
+set -e
 docker pull registry.sonata-nfv.eu:5000/son-yo-gen-bss
 docker run -d --name son-bss -p 25001:1337 -p 25002:1338 --log-driver=gelf --log-opt gelf-address=udp://10.31.11.37:12900 registry.sonata-nfv.eu:5000/son-yo-gen-bss grunt serve:integration_tests --gkApiUrl=http://sp.int3.sonata-nfv.eu:32001 --suite=unitTests --debug
 #docker exec -t -d son-bss grunt protractor_webdriver protractor:run --suite=unit
