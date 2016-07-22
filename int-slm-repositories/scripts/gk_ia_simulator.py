76c6bd174575ebf391d80944a5ad4df12c86dfbe
Adrián Roselló <adrian.rosello@i2cat.net>
2016-07-22 10:47:22 +0200

    Add comments to the code.

--- int-slm-repositories/scripts/gk_ia_simulator.py
+++ int-slm-repositories/scripts/gk_ia_simulator.py
@@ -2,17 +2,18 @@ import pika
 import yaml
 import threading
 import sys
-import json
 
+# The topic to which service instantiation requests
+# of the GK are published
 SERVICE_CREATE_TOPIC = 'service.instances.create'
-
-
 # The topic to which available vims are published
 INFRA_ADAPTOR_AVAILABLE_VIMS = 'infrastructure.management.compute.list'
 # The topic to which service instance deploy replies of the Infrastructure Adaptor are published
 INFRA_ADAPTOR_INSTANCE_DEPLOY_REPLY_TOPIC = "infrastructure.service.deploy"
 
 
+# This method reads the VNFDs and NSD from file and returns a dictionary containing them. The network service
+# consists of three network functions: firewall, iperf and tcpdump.
 def buildRequest():
 
     path_descriptors = 'resources/test_descriptors/'
@@ -31,6 +32,9 @@ def buildRequest():
     return yaml.dump(service_request)
 
 
+# Methiod simulating the GK. It creates a RabbitMQ connection and sends the GK request under the
+# 'service.instances.create' topic. After that, it waits for replies with same topic, consuming the
+# channels queue.
 def send_gk_request():
     request = buildRequest()
 
@@ -55,7 +59,9 @@ def send_gk_request():
 
     channel.start_consuming()
 
-
+# Callback method of the GK called when a message with 'service.instances.create' topic. It waits until the response either:
+# - indicates that the network service is deployed: it finishes the script.
+# - indicates that there was an error deploying the network service: it finishes the script with an error.
 def gk_receive(ch, method, properties, body):
 
     message = body.decode("utf-8")
@@ -69,7 +75,8 @@ def gk_receive(ch, method, properties, body):
         print ("Test failed.")
         sys.exit(-1)
 
-
+# Method simulating the IA. It creates a RabbitMQ connection and waits for requests with
+# 'infrastructure.management.compute.list'
 def listen_resource_availability():
 
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -91,6 +98,8 @@ def listen_resource_availability():
 
     channel.start_consuming()
 
+# Method simulating the IA. It creates a RabbitMQ connection and waits for requests with
+# 'infrastructure.service.deploy' topic.
 def listen_deploy_ns():
 
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -112,6 +121,11 @@ def listen_deploy_ns():
 
     channel.start_consuming()
 
+# Main method. Creates 3 threads:
+# - 1 thread for simulating the GK, which does the deployment request and waits for a response.
+# - 1 therad for IA, waiting for requests for compute resoources availabilty
+# - 1 thread for IA, waiting for requests for service deployment.
+
 def main():
     t = threading.Thread(target=listen_resource_availability)
     t.daemon = True
@@ -127,6 +141,9 @@ def main():
     t_gk.join()
 
 
+# Callback method called when a message with 'infrastructure.management.compute.list' topic is sent.
+# It reads from file the list of available vims (which are meaningless, it's only for testing) and
+# returns them through RabbitMQ with the same topic. This response should be read by SLM.
 def available_vims(ch, method, properties, body):
     message = body.decode("utf-8")
     if message == '{}':
@@ -135,6 +152,9 @@ def available_vims(ch, method, properties, body):
         ch.basic_publish(exchange='son-kernel', routing_key=INFRA_ADAPTOR_AVAILABLE_VIMS, body=yaml.dump(dict), properties=pika.BasicProperties(correlation_id=properties.correlation_id))
 
 
+# Callback method called when a message with 'infrastructure.service.deploy' topic is sent.
+# It reads the nsr and vnfrs whoch the IA should build after deploying the service, and send them
+# through the RabbitMQ connection with the same topic. This response should be read by SLM.
 def deployment_request_received(ch, method, properties, body):
 
     msg = yaml.load(body)
@@ -142,16 +162,6 @@ def deployment_request_received(ch, method, properties, body):
         ia_nsr   = yaml.load(open('resources/test_records/ia-nsr.yml','r'))
         #print ("Sending message: " + yaml.dump(ia_nsr))
         ch.basic_publish(exchange='son-kernel', routing_key=INFRA_ADAPTOR_AVAILABLE_VIMS, body=yaml.dump(ia_nsr), properties=pika.BasicProperties(correlation_id=properties.correlation_id))
-
-
-def is_json(message):
-    try:
-        json_object = json.loads(message)
-        return True
-    except ValueError as e:
-        return False
-
-
 
 if __name__ == '__main__':
     main()
