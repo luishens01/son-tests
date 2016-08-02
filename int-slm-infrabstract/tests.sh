840c778869d78d6a448867543f5f7f1f73f70cf9
DarioValocchi <dario.valocchi@gmail.com>
2016-08-02 15:37:10 +0200

    Made a separate script to call in the 'finally' block of the Jenskinsfile, so to clean the OpenStack env also when errors occur

--- int-slm-infrabstract/tests.sh
+++ int-slm-infrabstract/tests.sh
@@ -7,10 +7,3 @@ set -x
 docker run -it --rm -e broker_host=amqp://guest:guest@10.31.11.36:5672/%2F --name int_slm_ia_trigger slm_ia_trigger
 
 
-export OS_USERNAME=op_sonata
-export OS_PASSWORD=op_s0n@t@
-export OS_TENANT_NAME=op_sonata
-export OS_AUTH_URL=http://openstack.sonata-nfv.eu:5000/v2.0/
-
-
-heat stack-delete -y $(heat stack-list | head -n-1 | tail -n+4 | awk '{print $2}')
