71cbdb7ea07052fce293b469dc40e3ba127dc016
Daniel Guija <dani.guija@i2cat.net>
2016-11-15 12:26:02 +0000

    Added int. test for son-packages (disabled)

--- int-gtkpkg-sp-catalogue/tests5.sh
+++ int-gtkpkg-sp-catalogue/tests5.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#printf "\n\n======== POST SON Package to Catalogue ========\n\n\n"
+#resp=$(curl -qSfsw '\n%{http_code}' -H 'content-disposition: attachment; filename=sonata_example.son' \
+#  -H 'content-type: application/zip' \
+#  -F "package=@resources/sonata-demo.son" -X POST http://sp.int3.sonata-nfv.eu:4002/catalogues/son-packages)
+#echo $resp
+
+#package=$(echo $resp | grep "uuid")
+#echo "UUID: $package"
+
+#code=$(echo "$resp" | tail -n1)
+#echo "Code: $code"
+
 printf "\n\n======== GET Package UUID from Gatekeeper  ========\n\n\n"
 resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/packages) 2>/dev/null
 echo $resp
@@ -22,4 +34,28 @@ if [[ $code != 200 ]] ;
     exit -1
 fi
 
+printf "\n\n======== GET SON Package UUID from Catalogue  ========\n\n\n"
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:4002/catalogues/son-packages) 2>/dev/null
+echo $resp
+
+package=$(echo "$resp" | head -n-1)
+uuid=$(echo $package  |  python -mjson.tool | grep "uuid" | awk -F'[=:]' '{print $2}' | sed 's/\"//g' | tr -d ',' | tr -d '[:space:]')
+echo "UUID: $uuid"
+
+#printf "\n\n======== GET SON Package from Catalogue ========\n\n\n"
+#url=$(echo http://sp.int3.sonata-nfv.eu:32001/packages/${uuid})
+#url=$(echo http://sp.int3.sonata-nfv.eu:4002/catalogues/son-packages/${uuid})
+#echo "URL: $url"
+#resp=$(curl -qSfsw '\n%{http_code}' -X GET http://sp.int3.sonata-nfv.eu:32001/packages/${uuid}) 2>/dev/null
+#resp=$(curl -qSfsw '\n%{http_code}' -X GET http://sp.int3.sonata-nfv.eu:4002/catalogues/son-packages/${uuid}) 2>/dev/null
+#echo $resp
+
+#code=$(echo "$resp" | tail -n1)
+#echo "Code: $code"
+
+#if [[ $code != 200 ]] ;
+#  then
+#    echo "Error: Response error $code"
+#    exit -1
+#fi
 
