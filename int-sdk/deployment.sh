69e427b436b47cb1f419c22979ca8d6a45f0389d
Felipe Vicens <felipe.vicens@atos.net>
2017-01-25 19:39:03 +0100

    Update deployment.sh

--- int-sdk/deployment.sh
+++ int-sdk/deployment.sh
@@ -10,7 +10,7 @@ sudo apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y build-e
 sudo pip install flask  # for son-emu - not sure why flask always makes trouble when installed from setup.py
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
-echo DOCKER_OPTS=\"--insecure-registry registry.sonata-nfv.eu:5000 -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375\" | sudo tee /etc/default/docker
+#echo DOCKER_OPTS=\"--insecure-registry registry.sonata-nfv.eu:5000 -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375\" | sudo tee /etc/default/docker
 sudo service docker restart
 docker login -u sonata-nfv -p s0n@t@ registry.sonata-nfv.eu:5000
 
