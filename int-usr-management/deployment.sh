682c334119e634d1c17a6441aad5ff974ebc6f6e
srodriguezOPT <srodriguez@optaresolutions.com>
2017-03-22 11:26:14 +0100

    int-usr-management

--- int-usr-management/deployment.sh
+++ int-usr-management/deployment.sh
@@ -2,5 +2,5 @@
 cd ./bss_resources
 #curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 #sudo apt-get install -y nodejs
-sudo npm install
-sudo node ./node_modules/protractor/bin/webdriver-manager update
+npm install
+node ./node_modules/protractor/bin/webdriver-manager update
