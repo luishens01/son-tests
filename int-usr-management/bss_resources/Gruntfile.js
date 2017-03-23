ba2ad30f2179eca94e3f8df9b6dc8e33af046cc7
srodriguezOPT <srodriguez@optaresolutions.com>
2017-03-23 09:50:24 +0100

    int-usr-management

--- int-usr-management/bss_resources/Gruntfile.js
+++ int-usr-management/bss_resources/Gruntfile.js
@@ -74,6 +74,7 @@ module.exports = function(grunt) {
 
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);		
 		
-	grunt.registerTask('test', ['protractor_webdriver', 'protractor:run', 'watch:protractor']);
+	//grunt.registerTask('test', ['protractor_webdriver', 'protractor:run', 'watch:protractor']);
+	grunt.registerTask('test', ['protractor_webdriver', 'protractor:run']);
 		
 }; 
