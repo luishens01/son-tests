dcd6e79dc00593b5e7f6df7da7a090cb281bfd73
srodriguezOPT <srodriguez@optaresolutions.com>
2017-03-24 09:41:00 +0100

    int-bss-gkeeper updated

--- int-bss-gkeeper/deployment.sh
+++ int-bss-gkeeper/deployment.sh
@@ -2,7 +2,12 @@
 set -x
 set -e
 #DEPLOYMENT
-#
+
+cd int-bss-gkeeper/resources
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+npm install
+node ./node_modules/protractor/bin/webdriver-manager update
 ## -- insert NSD/VNFD
 int-bss-gkeeper/scripts/postCatalogueSampleDescriptors.sh
 int-bss-gkeeper/scripts/postGatekeeperSampleRequest.sh
