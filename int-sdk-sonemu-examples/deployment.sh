e0db524b0ac283e97a82f41c51567119bcf0cfe8
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-28 11:12:24 +0200

    Fix proper call ordering

--- int-sdk-sonemu-examples/deployment.sh
+++ int-sdk-sonemu-examples/deployment.sh
@@ -3,10 +3,10 @@ set -e
 set -x
 
 #### PREPARE ENVIRONMENT ####
-sudo ln -sf $(realpath ../int-sdk/utils/print-header.sh) /usr/bin/printheader
-printheader "Prepare Environment"
 sudo apt-get update -y -qq
 sudo apt-get install -y git realpath build-essential
+sudo ln -sf $(realpath ../int-sdk/utils/print-header.sh) /usr/bin/printheader
+printheader "Prepare Environment"
 sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y docker-engine
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
