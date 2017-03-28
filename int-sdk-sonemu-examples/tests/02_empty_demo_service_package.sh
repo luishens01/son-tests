f57cdbe03db4445892308a334cb65f37de04e063
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-28 16:12:08 +0200

    Starting to implement the first test

---
+++ int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+
+printheader "https://github.com/sonata-nfv/son-emu/wiki/Example-2"
+
+SONEMU() {
+    docker exec son-emu-int-sdk-pipeline ${1}
+}
+
+### Starting the topology
+docker exec son-emu-int-sdk-pipeline screen -L -S sonemu -d -m sudo python src/emuvim/examples/sonata_y1_demo_topology_1.py
+### Setup screen for immediate flusing
+docker exec son-emu-int-sdk-pipeline screen -S sonemu -X logfile flush 0
+### Wait for the cli to start
+docker exec son-emu-int-sdk-pipeline W '^*** Starting CLI:' 60s
+### Print nodes
+SONEMU Cmd 'nodes'
