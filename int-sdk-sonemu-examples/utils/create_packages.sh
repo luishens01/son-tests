e8d9a412563a640dd8ca78043efca21f0d57551f
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-28 15:54:25 +0200

    Fix incorrect directory path

--- int-sdk-sonemu-examples/utils/create_packages.sh
+++ int-sdk-sonemu-examples/utils/create_packages.sh
@@ -9,5 +9,5 @@ cd /tmp/workspace
 git clone https://github.com/sonata-nfv/son-examples.git
 cd son-examples/vnfs
 docker build -t sonatanfv/sonata-empty-vnf -f sonata-empty-vnf-docker/Dockerfile sonata-empty-vnf-docker
-docker exec son-cli-int-test bash -c 'cd service-projects && son-package --project sonata-empty-service-emu -n sonata-empty-service'
+docker exec son-cli-int-test bash -c 'cd son-examples/service-projects && son-package --project sonata-empty-service-emu -n sonata-empty-service'
 docker exec son-cli-int-test docker images
