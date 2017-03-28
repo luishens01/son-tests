2461f78cc61da3b76e59db517ef47e72722aa4db
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-28 11:20:58 +0200

    Trying to install docker-ce

--- int-sdk-sonemu-examples/deployment.sh
+++ int-sdk-sonemu-examples/deployment.sh
@@ -4,10 +4,15 @@ set -x
 
 #### PREPARE ENVIRONMENT ####
 sudo apt-get update -y -qq
-sudo apt-get install -y git realpath build-essential
+sudo apt-get install -y git realpath
 sudo ln -sf $(realpath ../int-sdk/utils/print-header.sh) /usr/bin/printheader
 printheader "Prepare Environment"
-sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y docker-engine
+sudo apt-get remove -y docker docker-engine || true
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y -qq
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y docker-ce
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
 docker login -u sonata-nfv -p s0n@t@ registry.sonata-nfv.eu:5000
