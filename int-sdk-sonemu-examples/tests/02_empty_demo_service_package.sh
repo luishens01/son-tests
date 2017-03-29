ac621263f30e7f9df48c4ae18429b4f2c9442d7e
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-29 16:22:16 +0200

    Completing the test

--- int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
+++ int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
@@ -29,12 +29,18 @@ SONCLI son-access -p emu push --deploy latest
 SONEMU son-emu-cli compute list
 SONEMU sync # avoid text overlapping
 
+SONEMU Cmd 'empty_vnf1 ifconfig && echo "... checked empty_vnf1"'
+SONEMU W "^... checked empty_vnf1"
 SONEMU Cmd 'empty_vnf2 ifconfig && echo "... checked empty_vnf2"'
 SONEMU W "^... checked empty_vnf2"
 SONEMU Cmd 'empty_vnf1 ping -v -c 2 empty_vnf2 && echo "... checked ping between empty_vnf1 and empty_vnf2"'
 SONEMU W "^... checked ping between empty_vnf1 and empty_vnf2"
+SONEMU Cmd 'empty_vnf1 ping -v -c 2 200.0.0.2 && echo "... checked ping between empty_vnf1 and 200.0.0.2"'
+SONEMU W "^... checked ping between empty_vnf1 and 200.0.0.2"
 SONEMU Cmd 'empty_vnf2 ping -v -c 2 empty_vnf1 && echo "... checked ping between empty_vnf2 and empty_vnf1"'
 SONEMU W "^... checked ping between empty_vnf2 and empty_vnf1"
+SONEMU Cmd 'empty_vnf2 ping -v -c 2 200.0.0.1 && echo "... checked ping between empty_vnf2 and 200.0.0.1"'
+SONEMU W "^... checked ping between empty_vnf2 and 200.0.0.1"
 
 SONEMU Cmd 'quit'
 W "^*** Done"
