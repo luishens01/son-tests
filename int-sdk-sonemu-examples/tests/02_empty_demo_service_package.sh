6e47e493633765ae9bfdb1e6e32479dc4c09c18f
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-29 13:59:41 +0200

    Fix using a absolute path when calling the topology

--- int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
+++ int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
@@ -9,7 +9,7 @@ SONEMU() {
 }
 
 ### Starting the topology
-docker exec son-emu-int-sdk-pipeline screen -L -S sonemu -d -m sudo python src/emuvim/examples/sonata_y1_demo_topology_1.py
+docker exec son-emu-int-sdk-pipeline screen -L -S sonemu -d -m sudo python /son-emu/src/emuvim/examples/sonata_y1_demo_topology_1.py
 ### Setup screen for immediate flusing
 docker exec son-emu-int-sdk-pipeline screen -S sonemu -X logfile flush 0
 ### Wait for the cli to start
