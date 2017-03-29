fca0d6e0a6a86fa5d0f7e85e95052181ff28a043
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-29 15:16:21 +0200

    Make the ping command more verbose

--- int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
+++ int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
@@ -29,9 +29,9 @@ SONEMU sync # avoid text overlapping
 
 SONEMU Cmd 'empty_vnf2 ifconfig && echo "... checked empty_vnf2"'
 SONEMU W "^... checked empty_vnf2"
-SONEMU Cmd 'empty_vnf1 ping -c 2 empty_vnf2 && echo "... checked ping between empty_vnf1 and empty_vnf2"'
+SONEMU Cmd 'empty_vnf1 ping -v -c 2 empty_vnf2 && echo "... checked ping between empty_vnf1 and empty_vnf2"'
 SONEMU W "^... checked ping between empty_vnf1 and empty_vnf2"
-SONEMU Cmd 'empty_vnf2 ping -c 2 empty_vnf1 && echo "... checked ping between empty_vnf2 and empty_vnf1"'
+SONEMU Cmd 'empty_vnf2 ping -v -c 2 empty_vnf1 && echo "... checked ping between empty_vnf2 and empty_vnf1"'
 SONEMU W "^... checked ping between empty_vnf2 and empty_vnf1"
 
 SONEMU Cmd 'quit'
