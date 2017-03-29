070dd92f532c108a3d7c37e04db7f56e01e2870f
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-29 16:33:02 +0200

    Fix trying to get around some weird mininet substitution

--- int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
+++ int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
@@ -33,9 +33,9 @@ SONEMU Cmd 'empty_vnf1 ifconfig && echo "... checked empty_vnf1"'
 SONEMU W "^... checked empty_vnf1"
 SONEMU Cmd 'empty_vnf2 ifconfig && echo "... checked empty_vnf2"'
 SONEMU W "^... checked empty_vnf2"
-SONEMU Cmd 'empty_vnf1 ping -v -c 2 empty_vnf2 && echo "... checked ping between empty_vnf1 and empty_vnf2"'
-SONEMU W "^... checked ping between empty_vnf1 and empty_vnf2"
-SONEMU Cmd 'empty_vnf1 ping -v -c 2 200.0.0.2 && echo "... checked ping between empty_vnf1 and 200.0.0.2"'
+SONEMU Cmd 'empty_vnf1 ping -v -c 2 empty_vnf2 && echo "... checked ping between Empty_vnf1 and Empty_vnf2"'
+SONEMU W "^... checked ping between Empty_vnf1 and Empty_vnf2"
+SONEMU Cmd "empty_vnf1 ping -v -c 2 200.0.0.2 && echo '... checked ping between empty_vnf1 and 200.0.0.2'"
 SONEMU W "^... checked ping between empty_vnf1 and 200.0.0.2"
 SONEMU Cmd 'empty_vnf2 ping -v -c 2 empty_vnf1 && echo "... checked ping between empty_vnf2 and empty_vnf1"'
 SONEMU W "^... checked ping between empty_vnf2 and empty_vnf1"
