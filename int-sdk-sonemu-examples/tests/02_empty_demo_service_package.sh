30f53cd388ffaa115c416748d8bc2d4cf5640d7d
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-03-29 16:45:27 +0200

    Fix renaming vnf to avoid substitution

--- int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
+++ int-sdk-sonemu-examples/tests/02_empty_demo_service_package.sh
@@ -33,14 +33,15 @@ SONEMU Cmd 'empty_vnf1 ifconfig && echo "... checked empty_vnf1"'
 SONEMU W "^... checked empty_vnf1"
 SONEMU Cmd 'empty_vnf2 ifconfig && echo "... checked empty_vnf2"'
 SONEMU W "^... checked empty_vnf2"
+### Warning: while executing the echo command, the name of a nvf is substituted by its ip
 SONEMU Cmd 'empty_vnf1 ping -v -c 2 empty_vnf2 && echo "... checked ping between Empty_vnf1 and Empty_vnf2"'
 SONEMU W "^... checked ping between Empty_vnf1 and Empty_vnf2"
-SONEMU Cmd "empty_vnf1 ping -v -c 2 200.0.0.2 && echo '... checked ping between empty_vnf1 and 200.0.0.2'"
-SONEMU W "^... checked ping between empty_vnf1 and 200.0.0.2"
-SONEMU Cmd 'empty_vnf2 ping -v -c 2 empty_vnf1 && echo "... checked ping between empty_vnf2 and empty_vnf1"'
-SONEMU W "^... checked ping between empty_vnf2 and empty_vnf1"
-SONEMU Cmd 'empty_vnf2 ping -v -c 2 200.0.0.1 && echo "... checked ping between empty_vnf2 and 200.0.0.1"'
-SONEMU W "^... checked ping between empty_vnf2 and 200.0.0.1"
+SONEMU Cmd 'empty_vnf1 ping -v -c 2 200.0.0.2 && echo "... checked ping between Empty_vnf1 and 200.0.0.2"'
+SONEMU W "^... checked ping between Empty_vnf1 and 200.0.0.2"
+SONEMU Cmd 'empty_vnf2 ping -v -c 2 empty_vnf1 && echo "... checked ping between Empty_vnf2 and Empty_vnf1"'
+SONEMU W "^... checked ping between Empty_vnf2 and Empty_vnf1"
+SONEMU Cmd 'empty_vnf2 ping -v -c 2 200.0.0.1 && echo "... checked ping between Empty_vnf2 and 200.0.0.1"'
+SONEMU W "^... checked ping between Empty_vnf2 and 200.0.0.1"
 
 SONEMU Cmd 'quit'
 W "^*** Done"
