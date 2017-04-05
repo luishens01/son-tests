afce8d3424a77fa645c09f43cde5edfd40bef7c1
dang03 <dang03@hotmail.com>
2017-04-05 15:36:14 +0100

    Added basic integration tests

--- int-gtkusr-keycloak/tests2.sh
+++ int-gtkusr-keycloak/tests2.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
-printf "\n\n======== POST Service Registration form to GTKUSR ==\n\n\n"
-resp=$(curl -qSfsw '\n%{http_code}' -F "package=@int-gtkusr-keycloak/resources/xxxxxxx.son" -X POST http://http://sp.int3.sonata-nfv.eu:5600/api/v2/register/service)
+printf "\n\n======== POST User (Developer) Login to GTKUSR ==\n\n\n"
+creds=$(echo -n 'user01:1234' | base64)
+echo "Credentials: $creds"
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Basic $creds" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d '' -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/login/user)
 echo $resp
 
-clientid=$(echo $resp | grep "cliendId")
+#token=$(echo $resp | grep "access_token")
 
 code=$(echo "$resp" | tail -n1)
 echo "Code: $code"
 
-if [[ $code != 201 ]] ;
+if [[ $code != 200 ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+printf "\n\n======== POST User (Customer) Login to GTKUSR ==\n\n\n"
+creds=$(echo -n 'user02:1234' | base64)
+echo "Credentials: $creds"
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Basic $creds" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d '' -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/login/user)
+echo $resp
+
+#token=$(echo $resp | grep "access_token")
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 200 ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+printf "\n\n======== POST Service (Micro-service) Login to GTKUSR ==\n\n\n"
+creds=$(echo -n 'son-catalogue:1234' | base64)
+echo "Credentials: $creds"
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Basic $creds" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d '' -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/login/user)
+echo $resp
+
+#token=$(echo $resp | grep "access_token")
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 200 ]] ;
   then
     echo "Error: Response error $code"
     exit -1
--- int-gtkusr-keycloak/tests3.sh
+++ int-gtkusr-keycloak/tests3.sh
@@ -1,8 +1,71 @@
 #!/bin/bash
-printf "\n\n======== POST User Login to GTKUSR ==\n\n\n"
+printf "\n\n======== GET User (Developer) Authorize to GTKUSR ==\n\n\n"
+creds=$(echo -n 'user01:1234' | base64)
+echo "Credentials: $creds"
 
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Basic $creds" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d '' -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/login/user)
+echo $resp
 
-if [[ $code != 200 ]] ;
+token=$(echo $resp | grep "access_token")
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:5600/api/v1/authorize) 2>/dev/null
+echo $resp
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 20* ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+printf "\n\n======== GET User (Customer) Authorize to GTKUSR ==\n\n\n"
+creds=$(echo -n 'user02:1234' | base64)
+echo "Credentials: $creds"
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Basic $creds" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d '' -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/login/user)
+echo $resp
+
+token=$(echo $resp | grep "access_token")
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:5600/api/v1/authorize) 2>/dev/null
+echo $resp
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 40* ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+printf "\n\n======== GET User (Micro-service) Authorize to GTKUSR ==\n\n\n"
+creds=$(echo -n 'son-catalogue:1234' | base64)
+echo "Credentials: $creds"
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Basic $creds" \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d '' -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/login/user)
+echo $resp
+
+token=$(echo $resp | grep "access_token")
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:5600/api/v1/authorize) 2>/dev/null
+echo $resp
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 20* ]] ;
   then
     echo "Error: Response error $code"
     exit -1
