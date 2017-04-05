ca0c124008aa1d02828ca5400dcf0cf004881559
alfonso_egio <alfonso.egio@i2cat.net>
2017-04-05 14:18:33 +0200

    son-keycloak and son-gtkusr conjoint deployment schema

--- int-gtkusr-keycloak/deployment.sh
+++ int-gtkusr-keycloak/deployment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ###WORK IN PROGRESS
 #DEPLOYMENT
-# export DOCKER_HOST="tcp://sp.int3.sonata-nfv.eu:2375"
-export DOCKER_HOST="unix:///var/run/docker.sock"
+export DOCKER_HOST="tcp://sp.int3.sonata-nfv.eu:2375"
+# export DOCKER_HOST="unix:///var/run/docker.sock"
 
 # MONGODB (USER_MANAGEMENT) Not implemented yet
 # Clean database
@@ -15,6 +15,18 @@ export DOCKER_HOST="unix:///var/run/docker.sock"
 #  sleep 1 && echo -n .; # waiting for mongo
 #done;
 
+function wait_for_web() {
+    until [ $(curl --connect-timeout 15 --max-time 15 -k -s -o /dev/null -I -w "%{http_code}" $1) -eq $2 ]; do
+	sleep 2 && 	echo -n .;
+
+	let retries="$retries+1"
+	if [ $retries -eq 12 ]; then
+	    echo "Timeout waiting for $2 status on $1"
+	    exit 1
+	fi
+    done
+}
+
 # ADAPTER - GTKUSR (GATEKEEPER)
 # Removing
 echo Stopping and removing containers
@@ -23,24 +35,20 @@ docker rm -fv son-gtkusr &&
 docker stop sonata-keycloak &&
 docker rm -fv sonata-keycloak &&
 sleep 5
-# Starting
-# docker run -d -p 5600:5600 --name son-gtkusr -e KEYCLOAK_ADDRESS=keycloak e- KEYCLOAK_PORT=5601 e- KEYCLOAK_PATH=auth e- SONATA_REALM=sonata e- CLIENT_NAME=adapter registry.sonata-nfv.eu:5000/son-gtksrv
-# Running docker compose using development deployment pushed images
-#     the images referenced in docker-compose-run.yml
-#     are tagged at the build stage https://jenkins.sonata-nfv.eu/view/DEVELOPMENT/job/son-gkeeper/configure
-#
+
 echo Building new containers
 docker-compose -f int-gtkusr-keycloak/resources/docker-compose_run.yml up -d
 
 echo Waiting for son-gtkusr ...
 # while ! nc -z sp.int3.sonata-nfv.eu 5600; do
-while ! nc -z localhost 5600; do
+while ! nc -z sp.int3.sonata-nfv.eu 5600; do
   sleep 1 && echo -n .; # waiting for gtkusr
 done;
 echo son-gtkusr Ready!
 
 echo Waiting for son-gtkusr-keycloak ...
 # while ! nc -z sp.int3.sonata-nfv.eu 5601; do
+wait_for_web sp.int3.sonata-nfv.eu:5601 200
 while ! nc -z localhost 5601; do
   sleep 1 && echo -n .; # waiting for gtkusr
 done;
