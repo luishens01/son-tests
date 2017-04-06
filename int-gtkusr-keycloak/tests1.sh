234b62976bd0283a37459d6c453c400e584498be
dang03 <dang03@hotmail.com>
2017-04-06 09:25:58 +0100

    Fixing tests

--- int-gtkusr-keycloak/tests1.sh
+++ int-gtkusr-keycloak/tests1.sh
@@ -2,7 +2,7 @@
 printf "\n\n======== POST User (Developer) Registration form to GTKUSR ==\n\n\n"
 resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
 -d "@int-gtkusr-keycloak/resources/user_d_registration.json" \
--X POST http://http://sp.int3.sonata-nfv.eu:5600/api/v2/register/user)
+-X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/register/user)
 echo $resp
 
 username=$(echo $resp | grep "username")
@@ -19,7 +19,7 @@ fi
 printf "\n\n======== POST User (Customer) Registration form to GTKUSR ==\n\n\n"
 resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
 -d "@int-gtkusr-keycloak/resources/user_c_registration.json" \
--X POST http://http://sp.int3.sonata-nfv.eu:5600/api/v2/register/user)
+-X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/register/user)
 echo $resp
 
 username=$(echo $resp | grep "username")
@@ -36,7 +36,7 @@ fi
 # printf "\n\n======== POST User (Developer+Customer) Registration form to GTKUSR ==\n\n\n"
 # resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
 # -d "@int-gtkusr-keycloak/resources/user_dc_registration.json" \
-# -X POST http://http://sp.int3.sonata-nfv.eu:5600/api/v2/register/user)
+# -X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/register/user)
 # echo $resp
 
 # username=$(echo $resp | grep "clientId")
@@ -53,7 +53,7 @@ fi
 printf "\n\n======== POST Service (Micro-service) Registration form to GTKUSR ==\n\n\n"
 resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
 -d "@int-gtkusr-keycloak/resources/service_registration.json" \
--X POST http://http://sp.int3.sonata-nfv.eu:5600/api/v2/register/service)
+-X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/register/service)
 echo $resp
 
 username=$(echo $resp | grep "clientId")
