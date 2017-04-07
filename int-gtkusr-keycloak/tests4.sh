75420f63c7dce479c7d85f2886d5c07a30a5af0b
dang03 <dang03@hotmail.com>
2017-04-07 10:58:44 +0100

    Fixing tests

--- int-gtkusr-keycloak/tests4.sh
+++ int-gtkusr-keycloak/tests4.sh
@@ -41,13 +41,13 @@ echo $resp
 code=$(echo "$resp" | tail -n1)
 echo "Code: $code"
 
-if [[ $code != 40* ]] ;
+if [[ $code != 20* ]] ;
   then
     echo "Error: Response error $code"
     exit -1
 fi
 
-printf "\n\n======== POST User (Micro-service) Logout to GTKUSR ==\n\n\n"
+printf "\n\n======== POST Service (Micro-service) Logout to GTKUSR ==\n\n\n"
 creds=$(echo -n 'son-catalogue:1234' | base64)
 echo "Credentials: $creds"
 
