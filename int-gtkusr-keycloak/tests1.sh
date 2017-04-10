04674a26fe5eb58ec02adf0c92aa6b4918b78435
dang03 <dang03@hotmail.com>
2017-04-10 15:29:16 +0100

    Added duplicated registration

--- int-gtkusr-keycloak/tests1.sh
+++ int-gtkusr-keycloak/tests1.sh
@@ -16,6 +16,23 @@ if [[ $code != 201 ]] ;
     exit -1
 fi
 
+printf "\n\n======== POST User (Developer) Duplicated Registration form to GTKUSR ==\n\n\n"
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
+-d "@int-gtkusr-keycloak/resources/user_d_registration.json" \
+-X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/register/user)
+echo $resp
+
+username=$(echo $resp | grep "username")
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 40* ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
 printf "\n\n======== POST User (Customer) Registration form to GTKUSR ==\n\n\n"
 resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
 -d "@int-gtkusr-keycloak/resources/user_c_registration.json" \
