04aef8f075c5c2ad321106ebc44dd815c993ee78
dang03 <dang03@hotmail.com>
2017-05-02 16:12:41 +0100

    Updated tests

--- int-usr-management/tests.sh
+++ int-usr-management/tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sleep 5
+
 cd int-usr-management/bss_resources
 ./node_modules/grunt/bin/grunt test --host=sp.int3.sonata-nfv.eu --port=25001 --protocol=https --suite=bssUserManagement
