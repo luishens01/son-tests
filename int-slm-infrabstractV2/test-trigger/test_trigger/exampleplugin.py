261f3cf0291864713d31ae447789963414479a84
root <thomas.soenen@ugent.be>
2017-05-15 11:39:42 +0200

    Two of the vnfds that the slm-ia test are using where missing the uuid field. Timeout of test reduced to 6 minutes.

--- int-slm-infrabstractV2/test-trigger/test_trigger/exampleplugin.py
+++ int-slm-infrabstractV2/test-trigger/test_trigger/exampleplugin.py
@@ -78,7 +78,7 @@ class DemoPlugin1(ManoBasePlugin):
         Plugin logic. Does nothing in our example.
         """
         LOG.debug('corr_id:'+self.correlation_id)
-        time.sleep(720)
+        time.sleep(360)
         LOG.debug('Timeout')
         os._exit(1)
 
