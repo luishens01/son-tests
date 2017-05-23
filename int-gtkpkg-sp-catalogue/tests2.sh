83ddd9f7431feed433489653552d6548acbb3164
dang03 <dang03@hotmail.com>
2017-05-23 14:30:25 +0100

    Added authentication methods and data to requests

--- int-gtkpkg-sp-catalogue/tests2.sh
+++ int-gtkpkg-sp-catalogue/tests2.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 #
 printf "\n\n======== GET Service from Gatekeeper  ========\n\n\n"
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/services) 2>/dev/null
+### Login phase
+echo "Authenticating to the Service Platform..."
+
+resp=$(curl -qSfsw '\n%{http_code}' -d '{"username":"demo","password":"demo"}' \
+http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions)
+echo $resp
+
+token=$(echo $resp | awk '{json=$1 FS $2 FS $3; print json}' | python -mjson.tool | grep "access_token" | awk -F ':[ \t]*' '{print $2}' | sed 's/,//g' | sed 's/"//g')
+echo "TOKEN="$token
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 200 ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+### Test phase
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/services) 2>/dev/null
 echo $resp
 
 code=$(echo "$resp" | tail -n1)
@@ -19,13 +40,29 @@ if [[ $code != 200 ]] ;
     exit -1
 fi
 
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/services/$uuid) 2>/dev/null
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/services/$uuid) 2>/dev/null
 echo $resp
 
 code=$(echo "$resp" | tail -n1)
 echo "Code: $code"
 
 if [[ $code != 200 ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+### Logout phase
+echo "Log-out of the Service Platform..."
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Bearer $token" \
+-X DELETE http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions) 2>/dev/null
+echo $resp
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 20* ]] ;
   then
     echo "Error: Response error $code"
     exit -1
--- int-gtkpkg-sp-catalogue/tests5.sh
+++ int-gtkpkg-sp-catalogue/tests5.sh
@@ -12,7 +12,27 @@
 #echo "Code: $code"
 
 printf "\n\n======== GET Package UUID and SON_PACKAGE_UUID from Gatekeeper  ========\n\n\n"
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/packages) 2>/dev/null
+### Login phase
+echo "Authenticating to the Service Platform..."
+
+resp=$(curl -qSfsw '\n%{http_code}' -d '{"username":"demo","password":"demo"}' \
+http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions)
+echo $resp
+
+token=$(echo $resp | awk '{json=$1 FS $2 FS $3; print json}' | python -mjson.tool | grep "access_token" | awk -F ':[ \t]*' '{print $2}' | sed 's/,//g' | sed 's/"//g')
+echo "TOKEN="$token
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 200 ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
+
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/packages) 2>/dev/null
 echo $resp
 
 package=$(echo "$resp" | head -n-1)
@@ -24,7 +44,8 @@ echo "UUID: $uuid"
 printf "\n\n======== GET Package from Gatekeeper ========\n\n\n"
 #url=$(echo http://sp.int3.sonata-nfv.eu:32001/packages/${uuid})
 #echo "URL: $url"
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/packages/${uuid}) 2>/dev/null
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/packages/${uuid}) 2>/dev/null
 echo $resp
 
 code=$(echo "$resp" | tail -n1)
@@ -40,7 +61,8 @@ printf "\n\n======== GET SON Package from Gatekeeper ========\n\n\n"
 #url=$(echo http://sp.int3.sonata-nfv.eu:32001/packages/${uuid})
 #echo "URL: $url"
 #resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/son-packages/${son_package_uuid}/) 2>/dev/null
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/packages/${uuid}/download) 2>/dev/null
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:32001/api/v2/packages/${uuid}/download) 2>/dev/null
 echo $resp
 
 code=$(echo "$resp" | tail -n1)
@@ -53,7 +75,8 @@ if [[ $code != 200 ]] ;
 fi
 
 printf "\n\n======== GET SON Package UUID from Catalogue  ========\n\n\n"
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -X GET http://sp.int3.sonata-nfv.eu:4002/catalogues/api/v2/son-packages) 2>/dev/null
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:4002/catalogues/api/v2/son-packages) 2>/dev/null
 echo $resp
 
 package=$(echo "$resp" | head -n-1)
@@ -63,7 +86,8 @@ echo "SON_PACKAGE_UUID: $son_package_uuid"
 printf "\n\n======== GET SON Package from Catalogue ========\n\n\n"
 #url=$(echo http://sp.int3.sonata-nfv.eu:4002/catalogues/son-packages/${uuid})
 #echo "URL: $url"
-resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/zip" -X GET http://sp.int3.sonata-nfv.eu:4002/catalogues/api/v2/son-packages/${son_package_uuid}) 2>/dev/null
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/zip" -H "Authorization: Bearer $token" \
+-X GET http://sp.int3.sonata-nfv.eu:4002/catalogues/api/v2/son-packages/${son_package_uuid}) 2>/dev/null
 echo $resp
 
 code=$(echo "$resp" | tail -n1)
@@ -75,3 +99,17 @@ if [[ $code != 200 ]] ;
     exit -1
 fi
 
+### Logout phase
+echo "Log-out of the Service Platform..."
+resp=$(curl -qSfsw '\n%{http_code}' -H "Authorization: Bearer $token" \
+-X DELETE http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions) 2>/dev/null
+echo $resp
+
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 20* ]] ;
+  then
+    echo "Error: Response error $code"
+    exit -1
+fi
