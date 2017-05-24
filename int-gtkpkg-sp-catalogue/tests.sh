95c071faaadd8966905527e398fbc6104a53454b
dang03 <dang03@hotmail.com>
2017-05-24 10:53:05 +0100

    Added user 'demo' registration

--- int-gtkpkg-sp-catalogue/tests.sh
+++ int-gtkpkg-sp-catalogue/tests.sh
@@ -18,3 +18,39 @@ if [[ $status_code != 20* ]] ;
     exit -1
 fi
 echo "Success: Gatekeeper found"
+
+# Integration user checks
+demo_reg_data() {
+    cat << EOF
+{"username": "demo",
+ "enabled": true,
+ "totp": false,
+ "emailVerified": false,
+ "firstName": "Demo",
+ "lastName": "User",
+ "email": "demo.user@email.com",
+ "credentials": [{"type": "password","value": "demo"}],
+ "requiredActions": [],
+ "federatedIdentities": [],
+ "attributes": {"userType": ["developer","customer"]},
+ "realmRoles": [],
+ "clientRoles": {},
+ "groups": []}
+EOF
+}
+
+# echo "$(demo_reg_data)"
+printf "\n\n======== POST Demo User (predefined) Registration form to GTKUSR ==\n\n\n"
+resp=$(curl -qSfsw '\n%{http_code}' -H "Content-Type: application/json" \
+-d "$(demo_reg_data)" \
+-X POST http://sp.int3.sonata-nfv.eu:5600/api/v1/register/user)
+echo $resp
+
+username=$(echo $resp | grep "username")
+code=$(echo "$resp" | tail -n1)
+echo "Code: $code"
+
+if [[ $code != 201 ]] ;
+  then
+    echo "Response $code"
+fi
