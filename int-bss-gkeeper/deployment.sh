066e65724ebc3308b8375d7231191c92978fbab8
srodriguezOPT <srodriguez@optaresolutions.com>
2017-08-24 13:15:27 +0200

    Old files deletion

--- int-bss-gkeeper/deployment.sh
+++ int-bss-gkeeper/deployment.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -x
 set -e
-#DEPLOYMENT
 
+#DEPLOYMENT
 cd int-bss-gkeeper/resources
-#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#sudo apt-get install -y nodejs
 npm install
 node ./node_modules/protractor/bin/webdriver-manager update
-## -- insert NSD/VNFD
-#../scripts/postCatalogueSampleDescriptors.sh
-#../scripts/postGatekeeperSampleRequest.sh
