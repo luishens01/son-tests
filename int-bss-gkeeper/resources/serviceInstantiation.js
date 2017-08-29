f7d0ad2dd312ab9d56aa694bb8b9bd92ebd46209
srodriguezOPT <srodriguez@optaresolutions.com>
2017-08-29 10:52:53 +0200

    browser.params.host

--- int-bss-gkeeper/resources/serviceInstantiation.js
+++ int-bss-gkeeper/resources/serviceInstantiation.js
@@ -28,17 +28,16 @@
  
 describe('SonataBSS Instantiates a Service', function() {
 
-    var requestId;
+    var requestId;    
     
     beforeEach(function() {
-        browser.driver.manage().window().maximize();
-        browser.get(browser.params.protocol+'://'+browser.params.hostname+':'+browser.params.port+'/#/login');
+        browser.driver.manage().window().maximize();        
+        browser.get(browser.params.protocol+'://'+browser.params.host+':'+browser.params.port+'/#/login');        
         browser.driver.findElement(by.id('username')).sendKeys('sonata');
         browser.driver.findElement(by.id('password')).sendKeys('1234');
         browser.driver.findElement(by.xpath('//button[. = "Login"]')).click();
         browser.driver.findElement(by.xpath("//a[@href='#/nSDs']")).click();
     });
-
 
     it('services list must not be empty', function() {
         var count = element.all(by.repeater('nSD in nSDs')).count();
